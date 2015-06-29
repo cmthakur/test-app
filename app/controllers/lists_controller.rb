c1040ade5bab863da95f5e9e4381f47a32063374
cmthakur <cmthakur.r2s@gmail.com>
2015-06-29 21:08:24 +0545

    add actions for list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,31 @@
+class ListsController < ApplicationController
+  before_filter :find_user
+
+  def index
+    @lists = @user.lists
+  end
+
+  def new
+    @list = @user.lists.new
+  end
+
+  def create
+    @list = @user.lists.new list_params
+    if @list.save
+      redirect_to user_lists_path(@user), notice: 'Congratulation! You have added a new list.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def find_user
+    @user = current_user
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
